0c2880ec01cb5a59b21798be9b3de71987d04ec6
Kirill Maksimov <kirill@orbs.com>
2020-03-19 14:19:38 +0200

    Refactor main

--- healtcheck.go
+++ healtcheck.go
@@ -1,8 +1,13 @@
 package healthcheck
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path"
 	"time"
 )
 
@@ -22,4 +27,37 @@ func Check(url string) (status string, err error) {
 	}
 
 	return
+}
+
+func Main() {
+	url := flag.String("url", "", "url to query")
+	output := flag.String("output", "", "path to file")
+
+	flag.Parse()
+
+	if *url == "" || *output == "" {
+		fmt.Println("url or output are missing")
+		os.Exit(1)
+	}
+
+	status, err := Check(*url)
+
+	result := map[string]interface{}{
+		"timestamp": time.Now(),
+		"status":    status,
+	}
+
+	if err != nil {
+		result["error"] = err.Error()
+	}
+
+	rawJSON, _ := json.MarshalIndent(result, "", "  ")
+
+	os.MkdirAll(path.Dir(*output), 0644)
+	ioutil.WriteFile(*output, rawJSON, 0644)
+
+	if err != nil {
+		fmt.Println(string(rawJSON))
+		os.Exit(1)
+	}
 }
